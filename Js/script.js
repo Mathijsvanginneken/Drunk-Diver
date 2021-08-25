4855c81fd8c841df1b6058aac9348fbc450d3dda
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-08-25 09:54:56 +0200

    done trash generation

--- Js/script.js
+++ Js/script.js
@@ -1,20 +1,35 @@
 
 const obstaclesArrayTrash = [];
-const player = new Player();
 const background = new Background(ctx);
+const player = new Player();
+
+let trashIntervalId = null
+let isStarted = true // Change it form the on-click event of the start button
 
 function letsPlay(){
+
+  if(!trashIntervalId && isStarted){
+    trashIntervalId = setInterval( ()=>{
+      const trash = new ObstacleTrash(
+        ctx,
+        Math.random() * canvas.width + 10,
+        0,
+        50,
+        50,
+        Math.ceil(Math.random() * 3)
+      );
+      obstaclesArrayTrash.push(trash)
+    }, 1000)}
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     player.update();
     background.draw();
     player.draw();
     obstaclesArrayTrash.forEach((eachObstacle) => {
         eachObstacle.draw();
-        eachObstacle.move();
-        
+        eachObstacle.move();    
     }),
     collisions();
-    console.log('Hey collision happening')
     
     requestAnimationFrame(letsPlay);
 }
